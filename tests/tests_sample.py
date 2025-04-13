450b6582b20f95587186d077604a1111c094f35d
Codesandmore <chackochanme@gmail.com>
2025-04-13 13:05:04 +0530

    draft

--- tests/tests_sample.py
+++ tests/tests_sample.py
@@ -1,5 +1,8 @@
 import unittest
-from tests.parseTest import TestParser
 
-if __name__ == '__main__':
+class TestSample(unittest.TestCase):
+    def test_example(self):
+        self.assertEqual(1 + 1, 2)
+
+if __name__ == "__main__":
     unittest.main()
